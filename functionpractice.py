b06619280de953053994274a0cb9813f67fc4812
harshdo <103445672+harshdo@users.noreply.github.com>
2023-10-13 14:36:13 +0530

    Add files via upload

---
+++ functionpractice.py
@@ -0,0 +1,26 @@
+def sayhello(name="abhi"):
+    print("hello "+name)
+
+
+sayhello("harsh dwivedi")
+
+# return value
+'''
+def sums(num1,num2):
+    total = num1 + num2
+    return total
+'''
+    #add = sums(80,20) (second way)
+    #print(add)
+
+
+    #print(sums(80,20)) #first way
+
+#Lambda function
+'''
+sums = lambda num1 , num2 : num1 +  num2
+
+print(sums('harsh','abhi'))
+'''
+
+
